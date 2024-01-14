e4e25fe4734815ecd636efd992480dbbbd6a116e
Shehryar Khan <shehryarkhattak001>
2024-01-14 19:23:21 +0500

    Add tailwindCss libraries and doing a little work on tailwindCss in it

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import ProjectSideBar from "./components/ProjectSideBar";
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <h1 className="my-8 text-center text-5xl font-bold"></h1>
+      <main className="h-screen my-8">
+        <ProjectSideBar />
+      </main>
+    </div>
+  );
 }
 
 export default App;
